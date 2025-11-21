16a0946217d151af40c030e7d6861443bb84469e
Emilio Zuñiga <emizc2004@gmail.com>
2025-11-21 08:17:31 -0600

    Implemetacion de errores (Primera version)

--- clases/Automata.py
+++ clases/Automata.py
@@ -22,8 +22,16 @@ class Automata:
         v_respuesta=""
         v_listTokens=self.m_obteListTokens()
 
+
+        # Estados de error:
+        # q999: Identificador no valido
+        # q998: Cadena numerica no valida
         if "q999" in v_listTokens:
+            print("Error(id): q999")
             v_respuesta= v_listTokens["q999"]
+        elif "q998" in v_listTokens:
+            print("Error(num): q998")
+            v_respuesta = v_listTokens["q998"]
         else:
             v_respuesta= self.m_tabuListTokens(v_listTokens)
 
@@ -58,14 +66,17 @@ class Automata:
 
         while v_linea<len(v_codigo) and not v_error:
             if (v_codigo[v_linea]!= " ") :
-                    v_error=self.m_veriErroLinea(v_codigo[v_linea],v_respuesta)
+                print("Linea: ",v_linea)
+                v_error=self.m_veriErroLinea(v_codigo[v_linea],v_respuesta)
 
             v_linea+=1
 
         if v_error:
             v_respuesta.clear()
-            v_respuesta["q999"]="Error in line "+str(v_linea)+" by invalid token "+self.a_tokeInvalido+" <--"
-
+            if v_error == "q999":
+                v_respuesta["q999"]="Error en la linea: "+str(v_linea)+". Por identificador no valido... "+self.a_tokeInvalido+" <--"
+            elif v_error == "q998":
+                v_respuesta["q998"]="Error en la linea: "+str(v_linea)+". Por valor numérico no valido... "+self.a_tokeInvalido+" <--"
 
         return v_respuesta
 
@@ -78,13 +89,13 @@ class Automata:
         v_caracter=""
         v_posicion=0
         v_lengLinea=len(p_linea)
-        while v_posicion<v_lengLinea and v_estaActual!="q999":
+        while v_posicion<v_lengLinea and (v_estaActual!="q999" and v_estaActual != "q998" ):
             v_caracter = ""
             v_tokeActual = ""
             v_estaActual = self.a_estaInicial
 
             #while automata
-            while v_posicion<v_lengLinea and self.a_matriz[v_estaActual]["a"]!="-" :
+            while v_posicion<v_lengLinea and v_estaActual in self.a_matriz and self.a_matriz[v_estaActual]["a"]!="-" :
                 v_caracter=p_linea[v_posicion]
                 v_tokeActual+=v_caracter
 
@@ -94,16 +105,20 @@ class Automata:
                 v_estaActual=self.a_matriz[v_estaActual][v_caracter]
                 v_posicion+=1
             #end while automata
-            print(v_estaActual,"+",v_tokeActual)
+            print("Estado actual: ",v_estaActual,"+ Token actual: ",v_tokeActual)
             if v_estaActual!="q0" :
 
-                if v_estaActual=="q999":
-                    v_respuesta=True
+                if v_estaActual=="q999" or v_estaActual=="q998":
+                    # Si empieza con digito y dio error, forzamos q998 (error numerico)
+                    if v_tokeActual and v_tokeActual[0].isdigit():
+                        v_respuesta="q998"
+                    else:
+                        v_respuesta=v_estaActual
                     self.a_tokeInvalido=v_tokeActual
                 else:
                     if v_caracter != "espacio" and (self.m_veriRequEspacio(v_estaActual,2000,5040) or v_estaActual in self.a_excepciones) :
                         v_posicion -= 1
-                        v_tokeActual = v_tokeActual[:-1] #a{ -> a
+                        v_tokeActual = v_tokeActual[:-1] 
 
                     if v_tokeActual not in p_listTokens:
                         if v_estaActual in self.a_constantes:
